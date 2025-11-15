420e289dc03e6e0bb3874f4fa9f4371173e61504
Adrian <adrian199909@gmail.com>
2025-11-15 19:10:13 +0100

    Add: React router error handling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,24 +1,73 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router/dom';
+import { useRouteError, isRouteErrorResponse, Outlet } from 'react-router';
+import './index.css';
 
 import Register from '@/views/Register';
-import Login from "@/views/Login";
+import Login from '@/views/Login';
 
 function Index() {
-    return <>matcha</>
+    return <>matcha</>;
+}
+
+function RootLayout() {
+    return (
+        <Outlet />
+    );
+}
+
+function NotFoundPage() {
+    return (
+        <div>
+            404 | Not Found
+        </div>
+    )
+}
+
+function RootErrorBoundary() {
+    const error = useRouteError();
+
+    if (isRouteErrorResponse(error)) {
+        return (
+            <>
+            <h1>
+                {error.status} {error.statusText}
+            </h1>
+            <p>{error.data}</p>
+            </>
+        );
+    } else if (error instanceof Error) {
+        return (
+            <div>
+            <h1>Error</h1>
+            <p>{error.message}</p>
+            <p>The stack trace is:</p>
+            <pre>{error.stack}</pre>
+            </div>
+        );
+    } else {
+        return <h1>Unknown Error</h1>;
+    }
 }
 
 const router = createBrowserRouter([
-    { path: "/", Component: Index },
-    { path: "login", Component: Login },
-    { path: "register", Component: Register }
+    {
+        path: '/',
+        element: <RootLayout />,
+        errorElement: <RootErrorBoundary />,
+        children: [
+            { index: true, element: <Index /> },
+            { path: 'login', element: <Login /> },
+            { path: 'register', element: <Register /> },
+            { path: '*', element: <NotFoundPage /> },
+        ],
+    },
 ]);
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <RouterProvider router={router} />
     </StrictMode>
-)
+);
