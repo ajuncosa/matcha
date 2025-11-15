f5a0d40fe1f4f1a7119ae5afe3471bb04d2da69f
Adrian <adrian199909@gmail.com>
2025-11-15 18:25:16 +0100

    Add: frontend router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 import './index.css'
-import App from './App.tsx'
+
+import Register from '@/views/Register';
+import Login from "@/views/Login";
+
+function Index() {
+    return <>matcha</>
+}
+
+const router = createBrowserRouter([
+    { path: "/", Component: Index },
+    { path: "login", Component: Login },
+    { path: "register", Component: Register }
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
 )
