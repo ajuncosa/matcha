abcefda2a806c48fa5c9d160d3f228dc11be695d
ajuncosa <anajuncosa97@gmail.com>
2025-12-01 20:46:21 +0100

    signup form

--- frontend/src/components/signup-form.tsx
+++ frontend/src/components/signup-form.tsx
@@ -13,14 +13,55 @@ import {
   FieldLabel,
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
+import { FolderMinus } from "lucide-react";
+import { useRef, useState } from "react";
 import { Link } from "react-router";
 
-function submit(e) {
-  e.preventDefault();
-  console.log("qwerqwer");
-}
+interface RegisterForm {
+  name: string,
+  email: string,
+  password: string;
+  confirm_password: string;
+};
 
 export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
+
+  const [form, setForm] = useState<RegisterForm>({
+    name: "",
+    email: "",
+    password: "",
+    confirm_password: ""
+  });
+  const [formError, setFormError] = useState<string>("")
+
+  function onFormChange(e) {
+    setForm({
+      ...form,
+      [e.target.id]: e.target.value
+    })
+  }
+
+  async function submit(e) {
+    e.preventDefault();
+    setFormError("");
+
+    if (form.password != form.confirm_password) {
+      setForm({...form, password: "", confirm_password: ""})
+    }
+
+    console.log("qwerqwe");
+    await fetch("localhost:81/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: form.name
+      })
+    });
+
+  }
+
   return (
     <Card {...props}>
       <CardHeader>
@@ -30,11 +71,11 @@ export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <form onSubmit={(e) => submit(e)}>
+        <form>
           <FieldGroup>
             <Field>
               <FieldLabel htmlFor="name">Full Name</FieldLabel>
-              <Input id="name" type="text" placeholder="John Doe" required />
+              <Input id="name" type="text" placeholder="John Doe" required onChange={onFormChange} />
             </Field>
             <Field>
               <FieldLabel htmlFor="email">Email</FieldLabel>
@@ -43,6 +84,7 @@ export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
                 type="email"
                 placeholder="m@example.com"
                 required
+                onChange={onFormChange}
               />
               <FieldDescription>
                 We&apos;ll use this to contact you. We will not share your email
@@ -51,7 +93,7 @@ export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
             </Field>
             <Field>
               <FieldLabel htmlFor="password">Password</FieldLabel>
-              <Input id="password" type="password" required />
+              <Input id="password" type="password" required onChange={onFormChange} value={form.password} />
               <FieldDescription>
                 Must be at least 8 characters long.
               </FieldDescription>
@@ -60,12 +102,13 @@ export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
               <FieldLabel htmlFor="confirm-password">
                 Confirm Password
               </FieldLabel>
-              <Input id="confirm-password" type="password" required />
+              <Input id="confirm_password" type="password" required onChange={onFormChange} value={form.confirm_password} />
               <FieldDescription>Please confirm your password.</FieldDescription>
             </Field>
             <FieldGroup>
               <Field>
-                <Button type="submit">Create Account</Button>
+                <span>{formError}</span>
+                <Button type="submit" onClick={submit}>Create Account</Button>
                 <FieldDescription className="px-6 text-center">
                   Already have an account? <Link to="/login">Sign in</Link>
                 </FieldDescription>
